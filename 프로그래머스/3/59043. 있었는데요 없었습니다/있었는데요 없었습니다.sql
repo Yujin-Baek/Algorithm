870291d9464ff4660a293713884943e6fefa47f4
Yujin Baek <87238236+Yujin-Baek@users.noreply.github.com>
2025-03-01 12:34:59 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,6 +1,5 @@
--- 코드를 입력하세요
 SELECT ins.animal_id, ins.name
-from animal_ins as ins
-join animal_outs as outs on ins.animal_id = outs.animal_id
+from animal_ins ins
+join animal_outs outs on ins.animal_id = outs.animal_id
 where outs.datetime < ins.datetime
-order by ins.datetime
+order by ins.datetime;
