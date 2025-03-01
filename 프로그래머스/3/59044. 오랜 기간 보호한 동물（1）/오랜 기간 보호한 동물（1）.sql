c2b61fffd08e17b839d8c2249e1e1cd1c013ec04
Yujin Baek <87238236+Yujin-Baek@users.noreply.github.com>
2025-03-01 12:50:39 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,4 +1,5 @@
 -- 코드를 입력하세요
-SELECT name, datetime 
-from (select animal_id, name, datetime from animal_ins order by datetime)
-where animal_id not in (select animal_id from animal_outs) and rownum <= 3
+SELECT name, datetime
+from animal_ins
+where animal_id not in (select animal_id from animal_outs)
+order by datetime limit 3
