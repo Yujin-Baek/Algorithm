355ab4fb441dee0066d04a6113f3c3bc42cfb9a9
Yujin Baek <87238236+Yujin-Baek@users.noreply.github.com>
2025-03-07 20:04:02 +0900

    [level 2] Title: 가장 큰 수, Time: 455.14 ms, Memory: 24.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,8 +1,17 @@
+from functools import cmp_to_key 
+
+def max_num(a, b):
+    if a+b > b+a:
+        return -1
+    elif b+a > a+b:
+        return 1
+    return 0
+    
 def solution(numbers):
     sarr = list(map(str, numbers))
     
     # 정렬 기준: 두 수를 이어붙였을 때 큰 순서대로 정렬
-    sarr.sort(key=lambda x: x * 10, reverse=True)
+    sarr.sort(key=cmp_to_key(max_num))
     
     # 만약 가장 큰 숫자가 0이면 전체가 0이므로 '0' 반환
     if sarr[0] == "0":
