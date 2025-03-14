60f2353eefda59d2143c9579211452601f46bb35
Yujin Baek <87238236+Yujin-Baek@users.noreply.github.com>
2025-03-14 14:40:26 +0900

    [level 2] Title: 더 맵게, Time: 1660.93 ms, Memory: 50.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,18 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    
+    while scoville[0] < K:
+        if len(scoville) <= 1:
+            return -1
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+        
+        result = first + second*2
+        
+        heapq.heappush(scoville, result)
+        answer += 1
+    
+    return answer
